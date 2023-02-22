f8aa7eb864ccd7b17df1d275787e2442fa436163
VikaKaluniuk <108570233+VikaKaluniuk@users.noreply.github.com>
2023-02-22 16:42:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+for (let i = 4; i <= 15; i++) {
+    console.log(i);
+  }
+  
+  for (let i = 0; i < 8; i++) {
+    console.log(8);
+  }
+  
+  let count = prompt("Скільки разів роздрукувати слово 'привіт'?");
+  
+  for (let i = 0; i < count; i++) {
+    console.log("привіт");
+  }
